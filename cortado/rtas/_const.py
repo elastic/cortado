bf535c00fe1672d91d8fb138064d84cf0d48e3ad
Sergey Polzunov <traut@users.noreply.github.com>
2024-11-01 18:02:27 +0100

    License header check (#9)

--- cortado/rtas/_const.py
+++ cortado/rtas/_const.py
@@ -1,3 +1,8 @@
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License
+# 2.0; you may not use this file except in compliance with the Elastic License
+# 2.0.
+
 DEFAULT_POWERSHELL_PATH = "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
 
 CATCH_ALL_IP = "0.0.0.0"
