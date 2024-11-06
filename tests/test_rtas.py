09fdc28d595dcd1510c68011dbcc74559c0ba05d
traut <sergey@polzunov.com>
2024-11-06 15:34:55 +0100

    Count files for the registry test

--- tests/test_rtas.py
+++ tests/test_rtas.py
@@ -3,6 +3,7 @@
 # 2.0; you may not use this file except in compliance with the Elastic License
 # 2.0.
 
+from pathlib import Path
 from cortado.rtas import load_all_modules, get_registry
 
 
@@ -12,8 +13,9 @@ def test_load_all_modules():
 
     registry = get_registry()
 
-    # Continuously changing number of RTAs
-    # Please make sure that the change corresponds with the number of new RTAs added
+    # NOTE: we assime here that 1 RTA == 1 file in `cortado/rtas` folder, which might not be always correct
+    rtas_directory_path = Path("cortado/rtas")
+    rta_files_found_gen = rtas_directory_path.glob("[!_]*.py")
+    rta_files_found_count = sum(1 for _ in rta_files_found_gen)
 
-    assert len(registry) == 590
-    
+    assert len(registry) == rta_files_found_count
